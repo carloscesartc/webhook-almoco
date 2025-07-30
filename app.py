231fc25f6006f134c5a894a7308e43a50763b886
carloscesartc <carloscesartc@gmail.com>
2025-07-30 18:16:48 -0300

    app.py

--- app.py
+++ app.py
@@ -32,4 +32,9 @@ def send_auto_reply(account_id, conversation_id):
     requests.post(url, json=data, headers=headers)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+   import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
